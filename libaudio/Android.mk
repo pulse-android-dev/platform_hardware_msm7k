997bab1d4adaef68d724ebab16fa06dc5e370927
Tom Giordano <tomgiordano83@gmail.com>
2010-10-02 21:16:37 +1000

    Revert "Changes from merging Code Aurora audio code."
    This reverts commit b180fd7d813cfb46e0c49d03adf47149eee1248f.

--- libaudio/Android.mk
+++ libaudio/Android.mk
@@ -48,7 +48,7 @@ LOCAL_CFLAGS += -fno-short-enums
 
 LOCAL_STATIC_LIBRARIES += libaudiointerface
 ifeq ($(BOARD_HAVE_BLUETOOTH),true)
-  LOCAL_SHARED_LIBRARIES += liba2dp libbinder
+  LOCAL_SHARED_LIBRARIES += liba2dp
 endif
 
 include $(BUILD_SHARED_LIBRARY)
